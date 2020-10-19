a40d266ee0035128f82dfd03ef9fda5b8bbd686d
IOCL Site <Guwahati5@KOL00005.europe.jacobs.com>
2020-10-19 20:33:55 +0530

    Implement prepared statement method to ensure database safety

--- submit.php
+++ submit.php
@@ -1,20 +1,34 @@
 <?php
 
+  // Check whether user came into page via desired route
   if (isset($_POST['submit']))
   {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $story = $_POST['story-text'];
-
+    // Database connection
     require_once './dbconnect.php';
 
-    $stmt = "INSERT INTO stories (storiesName, storiesEmail, storiesPhone, storiesStory) VALUES ('$name', '$email', '$phone', '$story')";
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $phone = mysqli_real_escape_string($con, $_POST['phone']);
+    $story = mysqli_real_escape_string($con, $_POST['story-text']);
 
-    mysqli_query($con, $stmt);
+    $sql = "INSERT INTO stories (storiesName, storiesEmail, storiesPhone, storiesStory) VALUES (?, ?, ?, ?)";
+
+    $stmt = mysqli_stmt_init($con);
+
+    if (!mysqli_stmt_prepare($stmt, $sql))
+    {
+      echo "SQL error";
+    }
+    else
+    {
+      mysqli_stmt_bind_param($stmt, 'ssss', $name, $email, $phone, $story);
+      mysqli_stmt_execute($stmt);
+    }
 
     header('location: danke.php');
   }
+
+
   else {
     echo "Failed to do thing";
     header("location: index.php");
