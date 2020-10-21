2a3af2b78d62b7001cd67c56633b6ccb57cb8c14
IOCL Site <Guwahati5@KOL00005.europe.jacobs.com>
2020-10-21 23:33:01 +0530

    Complete sample story page

--- story.php
+++ story.php
@@ -48,21 +48,151 @@
       </ul>
     </nav>
 
-  <h1 class="text-center">Batch of 2019-2020</h1>
+  <div class="page-content">
 
-  <hr class="centered-rule" />
+    <h1 class="text-center">Batch of 2019-2020</h1>
 
-  <img class="mx-auto d-block team-photo" alt="Group photo of 2019-20 batch of managers" src="https://images2.minutemediacdn.com/image/fetch/c_fill,g_auto,f_auto,h_395,w_600/https%3A%2F%2Fbvbbuzz.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2017%2F07%2F1128772726-850x560.jpeg" />
+    <hr class="centered-rule" />
 
-  <h2 class="story-heading">Heading</h2>
-  <div class="story-content">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis cras sed felis eget velit aliquet. Diam vulputate ut pharetra sit amet aliquam id. Faucibus et molestie ac feugiat sed lectus vestibulum. Blandit cursus risus at ultrices mi tempus imperdiet. Pretium lectus quam id leo in vitae turpis. Egestas diam in arcu cursus euismod quis viverra nibh. Id volutpat lacus laoreet non curabitur gravida arcu. Cursus sit amet dictum sit amet justo donec enim diam. Etiam tempor orci eu lobortis elementum nibh.
-    </p>
-    <p>
-      Aenean sed adipiscing diam donec adipiscing tristique risus nec feugiat. Eu turpis egestas pretium aenean pharetra magna ac. Dignissim cras tincidunt lobortis feugiat vivamus at. Gravida dictum fusce ut placerat orci nulla pellentesque. Parturient montes nascetur ridiculus mus mauris vitae ultricies. Est sit amet facilisis magna etiam tempor orci. Tristique risus nec feugiat in fermentum posuere urna nec. Senectus et netus et malesuada fames ac. Ante in nibh mauris cursus mattis molestie a iaculis. Augue eget arcu dictum varius duis at consectetur lorem donec. Eget mauris pharetra et ultrices neque.
-    </p>
+    <img class="mx-auto d-block team-photo" alt="Group photo of 2019-20 batch of managers" src="https://images2.minutemediacdn.com/image/fetch/c_fill,g_auto,f_auto,h_395,w_600/https%3A%2F%2Fbvbbuzz.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2017%2F07%2F1128772726-850x560.jpeg" />
+
+    <h2 class="section-heading">Heading</h2>
+
+    <hr class="side-rule ml-0" />
+
+    <div class="story-content">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis cras sed felis eget velit aliquet. Diam vulputate ut pharetra sit amet aliquam id. Faucibus et molestie ac feugiat sed lectus vestibulum. Blandit cursus risus at ultrices mi tempus imperdiet. Pretium lectus quam id leo in vitae turpis. Egestas diam in arcu cursus euismod quis viverra nibh. Id volutpat lacus laoreet non curabitur gravida arcu. Cursus sit amet dictum sit amet justo donec enim diam. Etiam tempor orci eu lobortis elementum nibh.
+      </p>
+      <p>
+        Aenean sed adipiscing diam donec adipiscing tristique risus nec feugiat. Eu turpis egestas pretium aenean pharetra magna ac. Dignissim cras tincidunt lobortis feugiat vivamus at. Gravida dictum fusce ut placerat orci nulla pellentesque. Parturient montes nascetur ridiculus mus mauris vitae ultricies. Est sit amet facilisis magna etiam tempor orci. Tristique risus nec feugiat in fermentum posuere urna nec. Senectus et netus et malesuada fames ac. Ante in nibh mauris cursus mattis molestie a iaculis. Augue eget arcu dictum varius duis at consectetur lorem donec. Eget mauris pharetra et ultrices neque.
+      </p>
+    </div>
+
+
+    <div class="container core-members-list">
+      <h2 class="section-heading">Core Team</h2>
+      <hr class="side-rule ml-0" />
+
+       <div class="row">
+
+         <div class="col-lg-6 col-sm-6 mt-4">
+           <div class="row">
+             <div class="col-3">
+               <img class="member-image" alt="Mr. Dog" src="https://www.ecellvnit.org/img/400x400/lakshya.jpeg"/>
+             </div>
+             <div class="col-9 my-auto">
+              <h3 class="member-name">Lakshya Shukla</h3>
+              <p class="member-role" >
+                President
+              </p>
+             </div>
+           </div>
+         </div>
+
+         <div class="col-lg-6 col-sm-6 mt-4">
+           <div class="row">
+             <div class="col-3">
+               <img class="member-image" alt="Mr. Dog" src="https://www.ecellvnit.org/img/400x400/kaustubh.jpeg"/>
+             </div>
+             <div class="col-9 my-auto">
+              <h3 class="member-name">Kaustubh Rajimwale</h3>
+              <p class="member-role" >
+                Vice President
+              </p>
+             </div>
+           </div>
+         </div>
+
+         <div class="col-lg-6 col-sm-6 mt-4">
+           <div class="row">
+             <div class="col-3">
+               <img class="member-image" alt="Mr. Dog" src="https://www.ecellvnit.org/img/400x400/Mithil.jpg"/>
+             </div>
+             <div class="col-9 my-auto">
+              <h3 class="member-name">Mithil Rao</h3>
+              <p class="member-role" >
+                Treasurer
+              </p>
+             </div>
+           </div>
+         </div>
+
+         <div class="col-lg-6 col-sm-6 mt-4">
+           <div class="row">
+             <div class="col-3">
+               <img class="member-image" alt="Mr. Dog" src="https://www.ecellvnit.org/img/400x400/bhojne.jpg"/>
+             </div>
+             <div class="col-9 my-auto">
+              <h3 class="member-name">Rushikesh Bhojne</h3>
+              <p class="member-role" >
+                Head Of Corporate
+              </p>
+             </div>
+           </div>
+         </div>
+
+         <div class="col-lg-6 col-sm-6 mt-4">
+           <div class="row">
+             <div class="col-3">
+               <img class="member-image" alt="Mr. Dog" src="https://www.ecellvnit.org/img/400x400/Archis.jpg"/>
+             </div>
+             <div class="col-9 my-auto">
+              <h3 class="member-name">Archis Bodhke</h3>
+              <p class="member-role" >
+                Head Of Corporate
+              </p>
+             </div>
+           </div>
+         </div>
+
+         <div class="col-lg-6 col-sm-6 mt-4">
+           <div class="row">
+             <div class="col-3">
+               <img class="member-image" alt="Mr. Dog" src="https://www.ecellvnit.org/img/400x400/ved.jpg"/>
+             </div>
+             <div class="col-9 my-auto">
+              <h3 class="member-name">Ved Kotpalliwar</h3>
+              <p class="member-role" >
+                Head Of Events
+              </p>
+             </div>
+           </div>
+         </div>
+
+         <div class="col-lg-6 col-sm-6 mt-4">
+           <div class="row">
+             <div class="col-3">
+               <img class="member-image" alt="Mr. Dog" src="https://www.ecellvnit.org/img/400x400/Gauri.jpg"/>
+             </div>
+             <div class="col-9 my-auto">
+              <h3 class="member-name">Gauri Dixit </h3>
+              <p class="member-role" >
+                Head Of Events
+              </p>
+             </div>
+           </div>
+         </div>
+
+         <div class="col-lg-6 col-sm-6 mt-4">
+           <div class="row">
+             <div class="col-3">
+               <img class="member-image" alt="Mr. Dog" src="https://www.ecellvnit.org/img/400x400/Ritika.jpeg"/>
+             </div>
+             <div class="col-9 my-auto">
+              <h3 class="member-name">Ritika Meena</h3>
+              <p class="member-role" >
+                Head Of Planning & Marketing
+              </p>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+
   </div>
+
+  <?php include("includes/footer_landing.php");?>
   <!--Bootstrap JS links-->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
